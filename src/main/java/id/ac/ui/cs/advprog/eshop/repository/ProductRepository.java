c1876bbf05a3f1a0b0ad4782261931526b21f744
tkgalon <z41d123@gmail.com>
2025-02-13 11:56:34 +0700

    Add ProductServece and Implementation

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -7,17 +7,15 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-@Repository // Memberitahu Spring kalau ini adalah komponen repository
+@Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
-    // Method menambahkan produk ke dalam repository
     public Product create(Product product) {
         productData.add(product);
         return product;
     }
 
-    // Method untuk mengambil semua produk dalam repository
     public Iterator<Product> findAll() {
         return productData.iterator();
     }
