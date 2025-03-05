0a3aa1cf625da687aa3b112e756029becc93c3ae
tkgalon <z41d123@gmail.com>
2025-03-05 22:19:18 +0700

    [RED] Add OrderRepository skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
@@ -18,7 +18,7 @@ public class OrderRepositoryTest {
 
     @BeforeEach
     void setUp() {
-        orderRepository = new orderRepository();
+        orderRepository = new OrderRepository();
 
         List<Product> products = new ArrayList<>();
         Product product1 = new Product();
