9eef7706c1d590fb2a70ddf8c64ae6fe76dd553d
tkgalon <z41d123@gmail.com>
2025-03-05 21:10:41 +0700

    [GREEN] Implement Order model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Arrays;
 import java.util.List;
 @Builder
 @Getter
@@ -12,14 +13,41 @@ public class Order {
     List<Product> products;
     Long orderTime;
     String author;
-    @Setter
     String status;
 
     public Order(String id, List<Product> products, long orderTime, String author) {
+        this.id = id;
+        this.orderTime = orderTime;
+        this.author = author;
+        this.status = "WAITING_PAYMENT";
 
+        if (products.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        else {
+            this.products = products;
+        }
     }
 
     public Order(String id, List<Product> products, long orderTime, String author, String status) {
+        this(id, products, orderTime, author);
 
+        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
+        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
+            throw new IllegalArgumentException();
+        }
+        else {
+            this.status = status;
+        }
+    }
+
+    public void setStatus(String status) {
+        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
+        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
+            throw new IllegalArgumentException();
+        }
+        else {
+            this.status = status;
+        }
     }
 }
